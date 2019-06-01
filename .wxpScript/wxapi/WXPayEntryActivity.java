6b5b568a394f2cbe021c7708ab5d4e8fd5e8c523
郑江荣 <362675035@qq.com>
2019-06-01 20:27:36 +0800

    update

---
+++ .wxpScript/wxapi/WXPayEntryActivity.java
@@ -0,0 +1,12 @@
+package appid.wxapi;
+
+import com.farwolf.wechat.WechatEntryActivity;
+
+/**
+ * Created by zhengjiangrong on 2018/5/31.
+ */
+public class WXPayEntryActivity extends WechatEntryActivity {
+
+
+
+}
